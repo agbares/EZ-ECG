60a50cf4e023e9d9cffec39dc6608476d17fe745
Antonio Bares <antonio.g.bares@gmail.com>
2018-12-10 14:02:49 -0800

    Project done

--- energia/main/main.ino
+++ energia/main/main.ino
@@ -3,7 +3,7 @@
 LiquidCrystal lcd(7, 2, 14, 3, 4, 19, 38);
 
 //int beat_old = 0;
-//int BPM = 0;
+int BPM = 0;
 //float beats[500];
 //int beatIndex;
 //
@@ -98,74 +98,106 @@ byte pixel[16][9];
 void setup() {
   // set up the lcd's number of columns and rows: 
   lcd.begin(16, 2);
+  
   pinMode(A1, INPUT);
+  pinMode(23, INPUT);
+  pinMode(24, INPUT);
+  
   Serial.begin(9600);
 
-
+  lcd.print("The Dr. is in.");
+  lcd.setCursor(0, 1);
+  lcd.print("BPM: ");
+  
+  // Init the pixels
   for (int i = 0; i < 16; i++) {
-    for (int j = 0; j < 8; j++) {
+    for (int j = 0; j < 9; j++) {
       pixel[i][j] = 0b00000;
     }
   }
 }
 
 void loop() {
-  lcd.setCursor(1, 1);
   // draw the little man, arms down:
-  
+  lcd.setCursor(5, 1);
 
-  int value = map(analogRead(A1), 0, 4095, 0, 7);
-//  Serial.println(value);
-//  byte chars[8] = {0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00001};
   
-//  lcd.createChar(1, chars);
-//  delay(100);
-//  lcd.write(1);
-//  draw(value);
+  int value = analogRead(A1);
+  Serial.println(value);
+//  int mappedValue = map(value, 0, 4095, 0, 7);
+  int lowerThreshold = 1790;
+  int upperThreshold = 2300;
+  int mappedValue = 0;
+  
+  if (value >= lowerThreshold && value <= upperThreshold) {
+    mappedValue = map(value, lowerThreshold - 1, upperThreshold + 1, 0, 7);
+    
+  } else if (value < lowerThreshold) {
+    mappedValue = 0;
+    
+  } else {
+    mappedValue = 7;
+  }
+    
 
-  for (int i = 0; i < 8; i++)
-    draw(i);
-  
+  if((digitalRead(23) == 1)||(digitalRead(24) == 1)){
+    
+    lcd.print("---");
+    draw(4);
+    
+  } else {
+    if (millis() % 10 == 0)
+      BPM = map(value, 0, 4095, 0, 200);
+    
+    if (BPM < 300) {
+      lcd.print("   ");
+      lcd.setCursor(5, 1);
+      lcd.print(BPM);
+    }
+
+    draw(mappedValue);
+  }
+
+  delay(100);
+
 }
 
-byte chars[8] = {0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000};
-
 void draw(int val) {
-  int _delay = 50;
+//  int _delay = 50;
 
   val = shiftPixel(val, pixel[0]);
   lcd.createChar(1, pixel[0]);
-  lcd.setCursor(0, 1);
+  lcd.setCursor(9, 1);
   lcd.write(1);
 
   val = shiftPixel(val, pixel[1]);
   lcd.createChar(2, pixel[1]);
-  lcd.setCursor(1, 1);
+  lcd.setCursor(10, 1);
   lcd.write(2);
 
   val = shiftPixel(val, pixel[2]);
   lcd.createChar(3, pixel[2]);
-  lcd.setCursor(2, 1);
+  lcd.setCursor(11, 1);
   lcd.write(3);
 
   val = shiftPixel(val, pixel[3]);
   lcd.createChar(4, pixel[3]);
-  lcd.setCursor(3, 1);
+  lcd.setCursor(12, 1);
   lcd.write(4);
 
   val = shiftPixel(val, pixel[4]);
   lcd.createChar(5, pixel[4]);
-  lcd.setCursor(4, 1);
+  lcd.setCursor(13, 1);
   lcd.write(5);
 
   val = shiftPixel(val, pixel[5]);
   lcd.createChar(6, pixel[5]);
-  lcd.setCursor(5, 1);
+  lcd.setCursor(14, 1);
   lcd.write(6);
   
   val = shiftPixel(val, pixel[6]);
   lcd.createChar(7, pixel[6]);
-  lcd.setCursor(6, 1);
+  lcd.setCursor(15, 1);
   lcd.write(7);
   
 //  for (int i = 0; i < 16; i++) {
@@ -176,7 +208,7 @@ void draw(int val) {
 //    lcd.write(1);
 //  }
 
-  delay(_delay);
+//  delay(_delay);
 }
 
 int shiftPixel(int val, byte pixel[]) {
@@ -215,6 +247,5 @@ int shiftPixel(int val, byte pixel[]) {
   nextVal--;
 
   nextVal = map(nextVal, 0, 7, 8, 0);
-  Serial.println(nextVal);
   return nextVal;
 }
